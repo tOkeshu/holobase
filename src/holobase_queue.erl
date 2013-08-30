63c2a37b14ab272d7e5d2efb0f400a05e596863b
Romain Gauthier <romain.gauthier@monkeypatch.me>
2013-08-30 18:57:35 +0200

    holobase_queue:push add a new item to the queue
    It also ensure that the queue have a fixed size

--- src/holobase_queue.erl
+++ src/holobase_queue.erl
@@ -1,6 +1,6 @@
 -module(holobase_queue).
 
--export([tr_queue/2]).
+-export([tr_queue/2, push/3]).
 
 tr_queue([], _Version) ->
     [];
@@ -8,4 +8,10 @@ tr_queue([{Version, _Op}|_Tail] = Queue, Version) ->
     [Op || {_Version, Op} <- Queue];
 tr_queue([{_Version, _Op}|Queue], Version) ->
     tr_queue(Queue, Version).
+
+push(Queue, Op, Version) when length(Queue) =:= 10 ->
+    [_Out|Queue2] = Queue,
+    push(Queue2, Op, Version);
+push(Queue, Op, Version) ->
+    lists:reverse([{Version, Op}|lists:reverse(Queue)]).
 
--- test/queue_tests.erl
+++ test/queue_tests.erl
@@ -11,3 +11,15 @@ no_tr_queue_test() ->
     TrQueue = holobase_queue:tr_queue(Queue, 5),
     ?assertEqual([], TrQueue).
 
+pust_test() ->
+    Queue = [],
+    NewQueue = holobase_queue:push(Queue, op, 0),
+    ?assertEqual([{0, op}], NewQueue).
+
+full_queue_push_test() ->
+    Queue = [{1, op1}, {2, op2}, {3, op3}, {4, op4}, {5, op5},
+             {6, op6}, {7, op7}, {8, op8}, {9, op9}, {10, op10}],
+    NewQueue = holobase_queue:push(Queue, op11, 11),
+    ?assertEqual([{2, op2}, {3, op3}, {4, op4}, {5, op5}, {6, op6}, {7, op7},
+                  {8, op8}, {9, op9}, {10, op10}, {11, op11}], NewQueue).
+
